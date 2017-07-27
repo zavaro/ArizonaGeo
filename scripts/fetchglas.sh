a0bb8a9c8e0103f1f1261af1e006669a616693a1
Matt Allbright <allbrigh@email.arizona.edu>
2017-07-27 13:44:55 +0200

    fetchglas
    * Fixed

--- scripts/fetchglas.sh
+++ scripts/fetchglas.sh
@@ -5,12 +5,12 @@
 cd ~/project/data
 
 for year in 2003 2004 2005 2006 2007 2008 2009; do
-	for month in 03; do
-		for day in 20; do
-			date=$year"."$month"."$day
+#	for month in 03; do
+#		for day in 20; do
+			date=$year".03.20"
 			echo "Downloading "$date
 			wget -r --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies --no-check-certificate --auth-no-challenge=on -r --reject xml,html --accept H5 -np -e robots=off https://n5eil01u.ecs.nsidc.org/GLAS/GLAH05.034/$date
 			echo -en '\n'
-		done
-	done
+#		done
+#	done
 done
